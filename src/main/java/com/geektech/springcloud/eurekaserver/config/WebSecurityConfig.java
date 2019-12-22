d567afb5873fc8b62785a659d04ed860411f2d5e
geektech <geekcarnegie@gmail.com>
2019-12-22 22:19:20 +0800

    Initial Project

---
+++ src/main/java/com/geektech/springcloud/eurekaserver/config/WebSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.geektech.springcloud.eurekaserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        // 关闭csrf
+        http.csrf().disable();
+    }
+}
